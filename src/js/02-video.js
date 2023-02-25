99e2eba0c91d2c070584a6d5c0ea789645ce8ecc
SashaHil <sashahil96@gmail.com>
2023-02-25 17:15:31 +0200

    виконав 2 завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+});
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
